066d6cb318031b85d5ac46dad1829a49d4b082a3
Mingocisse <36011578+Mingocisse@users.noreply.github.com>
2024-02-12 01:52:39 +0000

    initial setup

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +14,18 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+//All listing 
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings',  [
+        'heading'=>'Latest Listing',
+        'listings'=> Listing::all(),
+    ]);
+    });
+
+//Single listing
+Route::get('/listings/{id}', function($id){
+    return view('listing', [
+        'listing' => Listing::find($id)
+    ]);
 });
